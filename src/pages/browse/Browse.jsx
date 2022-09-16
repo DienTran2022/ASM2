f00bd899f68676ee2c10dfd684361ae16afc95cb
DienTran2022 <tranhuudien2@gmail.com>
2022-09-17 00:43:17 +0700

    Search

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -7,19 +7,21 @@ import BrowseStyle from "./Browse.module.css";
 
 function Browse() {
   return (
-    <div>
-      <Banner />
-      <div className={BrowseStyle["list"]}>
-        <MovieList fetchUrl={url.Banner} isLarge />
-        <MovieList title="Xu hướng" fetchUrl={url.Trending} />
-        <MovieList title="Xếp hạng cao" fetchUrl={url.TopRate} />
-        <MovieList title="Hành động" fetchUrl={url.Action} />
-        <MovieList title="Hài" fetchUrl={url.Comedy} />
-        <MovieList title="Kinh dị" fetchUrl={url.Horror} />
-        <MovieList title="Lãng mạn" fetchUrl={url.Romance} />
-        <MovieList title="Tài liệu" fetchUrl={url.Document} />
+    <React.Fragment>
+      <div>
+        <Banner />
+        <div className={BrowseStyle["list"]}>
+          <MovieList fetchUrl={url.Banner} isLarge />
+          <MovieList title="Xu hướng" fetchUrl={url.Trending} />
+          <MovieList title="Xếp hạng cao" fetchUrl={url.TopRate} />
+          <MovieList title="Hành động" fetchUrl={url.Action} />
+          <MovieList title="Hài" fetchUrl={url.Comedy} />
+          <MovieList title="Kinh dị" fetchUrl={url.Horror} />
+          <MovieList title="Lãng mạn" fetchUrl={url.Romance} />
+          <MovieList title="Tài liệu" fetchUrl={url.Document} />
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -4,11 +4,11 @@ import SearchStyle from "./Search.module.css";
 
 const Search = () => {
   return (
-    <div className={SearchStyle["app"]}>
+    <div className={SearchStyle["search"]}>
       <NavBar />
-      <div>
-        <div>
-          <input />
+      <div className={SearchStyle["app"]}>
+        <div className={SearchStyle["banner"]}>
+          <input className={SearchStyle["input"]} />
           <p className={SearchStyle["logo"]}>
             <svg
               className="svg-inline--fa fa-search fa-w-16"
@@ -23,11 +23,11 @@ const Search = () => {
               <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
             </svg>
           </p>
-          <hr></hr>
-          <div>
-            <button>RESET</button>
-            <button>SEARCH</button>
-          </div>
+        </div>
+        <hr className={SearchStyle["hr"]}></hr>
+        <div className={SearchStyle["button"]}>
+          <button>RESET</button>
+          <button>SEARCH</button>
         </div>
       </div>
     </div>
